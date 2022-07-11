74f17bbe0543d4d10ca1b3bfe716945a7c4356f4
Sardor979bek <37942959+Sardor979bek@users.noreply.github.com>
2022-07-11 15:00:33 -0500

    Update on StudentServiceImpl

--- backend/src/main/java/edu/miu/backend/controller/JobHistoryController.java
+++ backend/src/main/java/edu/miu/backend/controller/JobHistoryController.java
@@ -1,5 +1,6 @@
 package edu.miu.backend.controller;
 
+import edu.miu.backend.dto.JobHistoryDto;
 import edu.miu.backend.entity.JobHistory;
 import edu.miu.backend.service.JobHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +18,19 @@ public class JobHistoryController {
     private JobHistoryService jobHistoryService;
 
     @GetMapping("")
-    public ResponseEntity<List<JobHistory>> getAll(){
+    public ResponseEntity<List<JobHistoryDto>> getAll(){
         return ResponseEntity.ok().body(jobHistoryService.findAll());
     }
 
-    public ResponseEntity<JobHistory> getById(int id){
+    @GetMapping("/{id}")
+    public ResponseEntity<JobHistoryDto> getById(int id){
         return ResponseEntity.ok().body(jobHistoryService.findById(id));
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<JobHistoryDto> updateJobHistory(@RequestBody JobHistoryDto jobHistoryDto, @PathVariable int id){
+        return ResponseEntity.ok().body(jobHistoryService.update(jobHistoryDto, id));
+    }
+
 
 }
--- backend/src/main/java/edu/miu/backend/service/impl/JobHistoryServiceImpl.java
+++ backend/src/main/java/edu/miu/backend/service/impl/JobHistoryServiceImpl.java
@@ -1,27 +1,46 @@
 package edu.miu.backend.service.impl;
 
+import edu.miu.backend.dto.JobHistoryDto;
 import edu.miu.backend.entity.JobHistory;
 import edu.miu.backend.repo.JobHistoryRepo;
 import edu.miu.backend.service.JobHistoryService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class JobHistoryServiceImpl implements JobHistoryService {
 
     private final JobHistoryRepo jobHistoryRepo;
+    private final ModelMapper modelMapper;
 
 
     @Override
-    public List<JobHistory> findAll() {
-        return (List<JobHistory>) jobHistoryRepo.findAll();
+    public List<JobHistoryDto> findAll() {
+        List<JobHistory> histories =  (List<JobHistory>) jobHistoryRepo.findAll();
+        return histories.stream().map(jobHistory -> modelMapper.map(jobHistory, JobHistoryDto.class)).collect(Collectors.toList());
     }
 
     @Override
-    public JobHistory findById(int id) {
-        return jobHistoryRepo.findById(id).get();
+    public JobHistoryDto findById(int id) {
+        return modelMapper.map(jobHistoryRepo.findById(id).get(), JobHistoryDto.class);
+    }
+
+    @Override
+    public JobHistoryDto update(JobHistoryDto jobHistoryDto, int id) {
+        JobHistory jobHistory = jobHistoryRepo.findById(id).get();
+        JobHistory jobHistoryUpdate = modelMapper.map(jobHistoryDto, JobHistory.class);
+        jobHistory.setCompanyName(jobHistoryUpdate.getCompanyName());
+        jobHistory.setReasonToLeave(jobHistoryUpdate.getReasonToLeave());
+        jobHistory.setStartDate(jobHistoryUpdate.getStartDate());
+//        Todo:
+//        jobHistory.setTags();
+        ;
+        return modelMapper.map(jobHistoryRepo.save(jobHistory), JobHistoryDto.class);
     }
 }
